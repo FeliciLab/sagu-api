41e92da8e326c96b14f29d7f0e2b58d8032cf62a
Chicão Thiago <fthiagogv@gmail.com>
2020-08-28 10:32:27 -0300

    #7 Adicionando validação da página

--- app/Http/Controllers/ResidenciaMultiprofissional/TurmaSupervisorController.php
+++ app/Http/Controllers/ResidenciaMultiprofissional/TurmaSupervisorController.php
@@ -1,20 +1,34 @@
 <?php
+
 namespace App\Http\Controllers\ResidenciaMultiprofissional;
 
 use App\DAO\ResidenciaMultiprofissional\TurmaDAO;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use function response;
 
 
 class TurmaSupervisorController extends Controller
 {
-    public function turmasSupervisor(Request $request, TurmaDAO $turmaDAO, $page=null)
+    public function turmasSupervisor(Request $request, TurmaDAO $turmaDAO, $page = null)
     {
-        return \response()->json([
-            'turmas' => $turmaDAO->buscarTurmasSupervisor(
-                $request->get('usuario')->supervisorid,
-                $page
-            )
-        ]);
+        if ($page !== null && !ctype_digit($page)) {
+            return response()->json(
+                [
+                    'error' => true,
+                    'status' => 400,
+                    'message' => 'Atributo {page} não é um número inteiro'
+                ],
+                400
+            );
+        }
+        return response()->json(
+            [
+                'turmas' => $turmaDAO->buscarTurmasSupervisor(
+                    $request->get('usuario')->supervisorid,
+                    $page
+                )
+            ]
+        );
     }
 }

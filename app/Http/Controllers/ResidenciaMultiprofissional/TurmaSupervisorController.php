b27ada5b20232294619eb6b54f94dda2d3ebe569
Chicão Thiago <fthiagogv@gmail.com>
2020-08-28 11:15:00 -0300

    #7 Criadno trait de validação de paginas

--- app/Http/Controllers/ResidenciaMultiprofissional/TurmaSupervisorController.php
+++ app/Http/Controllers/ResidenciaMultiprofissional/TurmaSupervisorController.php
@@ -4,24 +4,21 @@ namespace App\Http\Controllers\ResidenciaMultiprofissional;
 
 use App\DAO\ResidenciaMultiprofissional\TurmaDAO;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\ResidenciaMultiprofissional\Traits\PaginationValidateRequest;
 use Illuminate\Http\Request;
 use function response;
 
 
 class TurmaSupervisorController extends Controller
 {
+    use PaginationValidateRequest;
+
     public function turmasSupervisor(Request $request, TurmaDAO $turmaDAO, $page = null)
     {
-        if ($page !== null && !ctype_digit($page)) {
-            return response()->json(
-                [
-                    'error' => true,
-                    'status' => 400,
-                    'message' => 'Atributo {page} não é um número inteiro'
-                ],
-                400
-            );
+        if ($this->validatePageParameter($page)) {
+            return $this->responsePaginationError();
         }
+
         return response()->json(
             [
                 'turmas' => $turmaDAO->buscarTurmasSupervisor(

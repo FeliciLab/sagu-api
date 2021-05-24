9e1a38cb55fe00380b599c10efd9c2c782ee527b
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2021-05-24 09:31:33 -0300

    #2 adicionando test auth sem o parametro senha

--- tests/ResidenciaMultiprofissional/AuthTest.php
+++ tests/ResidenciaMultiprofissional/AuthTest.php
@@ -14,4 +14,17 @@ class AuthTest extends TestCase
             'message' => 'Login Inválido',
         ]);
     }
+
+    public function testAuthSemParametroSenha()
+    {
+        $this->post('/auth', array(
+            'usuario' => env('USER_TEST')
+        ));
+
+        $this->seeStatusCode(401);
+        $this->seeJsonEquals([
+            'login' => 'false',
+            'message' => 'Login Inválido',
+        ]);
+    }
 }

ecd1b262ae91fe2c277616d88bf746ac55a9afec
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2022-06-29 13:14:08 -0300

    retorna turma em período vigente de inscrições

--- app/DAO/EnsinoPesquisaExtensao/TurmaDAO.php
+++ app/DAO/EnsinoPesquisaExtensao/TurmaDAO.php
@@ -10,22 +10,19 @@ class TurmaDAO
 {
     public function getTurmasPorOferta($ofertaId)
     {
-        $select = DB::select("SELECT * FROM acpofertaturma WHERE situacao = 'A' AND ofertacursoid = :ofertacursoid ORDER BY descricao", ['ofertacursoid' => $ofertaId]);
+        $select = DB::select("SELECT * FROM acpofertaturma WHERE situacao = 'A' AND ofertacursoid = :ofertacursoid AND NOW() BETWEEN datainicialinscricao AND datafinalinscricao ORDER BY descricao", 
+            [
+                'ofertacursoid' => $ofertaId
+            ]
+        );
 
         $turmas = [];
         foreach ($select as $_turma) {
             $turma = new Turma();
             $turma->id = $_turma->ofertaturmaid;
             $turma->descricao = $_turma->descricao;
-            $turma->minimoAlunos = $_turma->minimoalunos;
-            $turma->maximoAlunos = $_turma->maximoalunos;
-            $turma->dataInicial = $_turma->datainicialoferta;
-            $turma->dataFinal = $_turma->datafinaloferta;
             $turma->dataInicialInscricao = $_turma->datainicialinscricao;
             $turma->dataFinalInscricao = $_turma->datafinalinscricao;
-            $turma->dataInicialMatricula = $_turma->datainicialmatricula;
-            $turma->dataFinalMatricula = $_turma->datafinalmatricula;
-            $turma->situacao = $_turma->situacao;
             $turmas[] = $turma;
         }
 
--- app/Model/EnsinoPesquisaExtensao/Turma.php
+++ app/Model/EnsinoPesquisaExtensao/Turma.php
@@ -6,13 +6,6 @@ class Turma
 {
     public $id;
     public $descricao;
-    public $minimoAlunos;
-    public $maximoAlunos;
-    public $dataInicial;
-    public $dataFinal;
     public $dataInicialInscricao;
     public $dataFinalInscricao;
-    public $dataInicialMatricula;
-    public $dataFinalMatricula;
-    public $situacao;
 }

604379facff4c7836a17c099b3113f020fc85e5a
fpontef <sobralense@gmail.com>
2022-09-02 19:14:44 -0300

    refactor: alterado tratamento de excecoes

--- app/DAO/EnsinoPesquisaExtensao/CursoDAO.php
+++ app/DAO/EnsinoPesquisaExtensao/CursoDAO.php
@@ -10,31 +10,26 @@ class CursoDAO
 
     public function verificaInscricaoAtivaNaTurma($turmaid, $personid)
     {
-        try {
-
-            $queryInscricao = DB::select(
-                "SELECT inscricao.inscricaoid FROM acpinscricao AS inscricao
+        $queryInscricao = DB::select(
+            "SELECT inscricao.inscricaoid FROM acpinscricao AS inscricao
             INNER JOIN acpinscricaoturmagrupo AS IT
             ON IT.inscricaoid = inscricao.inscricaoid
             WHERE inscricao.personid = $personid
             AND inscricao.situacao = 'I'
             AND IT.ofertaturmaid = $turmaid"
-            );
+        );
 
-            $queryMatricula = DB::select(
-                "SELECT matricula.matriculaid FROM acpmatricula AS matricula
+        $queryMatricula = DB::select(
+            "SELECT matricula.matriculaid FROM acpmatricula AS matricula
             INNER JOIN acpinscricaoturmagrupo AS IT
             ON IT.inscricaoturmagrupoid = matricula.inscricaoturmagrupoid
             WHERE matricula.personid = $personid
             AND matricula.situacao = 'M'
             AND IT.ofertaturmaid = $turmaid"
-            );
+        );
 
-            if ($queryInscricao || $queryMatricula) {
-                throw new \Exception('Pessoa possui Inscrição ou Matrícula ativa.');
-            }
-        } catch (\Throwable $e) {
-            throw $e;
+        if ($queryInscricao || $queryMatricula) {
+            throw new Exception('Pessoa possui Inscrição ou Matrícula ativa.');
         }
 
         return false;
@@ -42,131 +37,126 @@ class CursoDAO
 
     public function getCursoDados($turmaId)
     {
-        try {
+        $cursoDados = DB::table('public.acpofertaturma')
+            ->where('public.acpofertaturma.ofertaturmaid', $turmaId)
+            ->where('public.acpofertaturma.situacao', 'A')
+            ->whereRaw('NOW() BETWEEN public.acpofertaturma.datainicialinscricao AND public.acpofertaturma.datafinalinscricao')
+            ->where('public.acpofertacurso.situacao', 'A')
+            ->join(
+                'public.acpofertacurso',
+                'public.acpofertaturma.ofertacursoid',
+                '=',
+                'public.acpofertacurso.ofertacursoid'
+            )
+            ->join(
+                'public.acpocorrenciacurso',
+                'public.acpofertacurso.ocorrenciacursoid',
+                '=',
+                'public.acpocorrenciacurso.ocorrenciacursoid'
+            )
+            ->join(
+                'public.acpcurso',
+                'public.acpocorrenciacurso.cursoid',
+                '=',
+                'public.acpcurso.cursoid'
+            )
+            ->join(
+                'public.acpperfilcurso',
+                'public.acpcurso.perfilcursoid',
+                '=',
+                'public.acpperfilcurso.perfilcursoid'
+            )
+            ->select(
+                'acpofertacurso.ofertacursoid',
+                'acpofertacurso.ocorrenciacursoid',
+                'acpofertacurso.descricao as oferta_descricao',
+                'acpofertacurso.situacao as oferta_situacao',
+                'acpocorrenciacurso.cursoid',
+                'acpocorrenciacurso.situacao as ocorrencia_situacao',
+                'acpcurso.perfilcursoid',
+                'acpcurso.numeroformalvagas',
+                'acpcurso.datainicio as curso_datainicio',
+                'acpcurso.datafim as curso_datafim',
+                'acpperfilcurso.modelodeavaliacaogeral',
+                'acpperfilcurso.modelodeavaliacaoseriado',
+                'acpperfilcurso.modelodeavaliacaomodulo',
+                'acpperfilcurso.permiteinscricaoporgrupo',
+                'acpperfilcurso.descricao as perfil_descricao',
+                'acpperfilcurso.ativo as perfil_ativo',
+                'acpofertaturma.ofertaturmaid',
+                'acpofertaturma.codigo as turma_codigo',
+                'acpofertaturma.descricao as turma_descricao',
+                'acpofertaturma.datainicialoferta',
+                'acpofertaturma.datafinaloferta',
+                'acpofertaturma.dataencerramento',
+                'acpofertaturma.datainicialaulas',
+                'acpofertaturma.datafinalaulas',
+                'acpofertaturma.datainicialinscricao',
+                'acpofertaturma.datafinalinscricao',
+                'acpofertaturma.datainicialmatricula',
+                'acpofertaturma.datafinalmatricula',
+                'acpofertaturma.minimoalunos',
+                'acpofertaturma.maximoalunos',
+                'acpofertaturma.centerid',
+                'acpofertaturma.unitid',
+                'acpofertaturma.habilitada',
+            )
+            ->first();
 
-            $cursoDados = DB::table('public.acpofertaturma')
-                ->where('public.acpofertaturma.ofertaturmaid', $turmaId)
-                ->where('public.acpofertaturma.situacao', 'A')
-                ->whereRaw('NOW() BETWEEN public.acpofertaturma.datainicialinscricao AND public.acpofertaturma.datafinalinscricao')
-                ->where('public.acpofertacurso.situacao', 'A')
-                ->join(
-                    'public.acpofertacurso',
-                    'public.acpofertaturma.ofertacursoid',
-                    '=',
-                    'public.acpofertacurso.ofertacursoid'
-                )
-                ->join(
-                    'public.acpocorrenciacurso',
-                    'public.acpofertacurso.ocorrenciacursoid',
-                    '=',
-                    'public.acpocorrenciacurso.ocorrenciacursoid'
-                )
-                ->join(
-                    'public.acpcurso',
-                    'public.acpocorrenciacurso.cursoid',
-                    '=',
-                    'public.acpcurso.cursoid'
-                )
-                ->join(
-                    'public.acpperfilcurso',
-                    'public.acpcurso.perfilcursoid',
-                    '=',
-                    'public.acpperfilcurso.perfilcursoid'
-                )
-                ->select(
-                    'acpofertacurso.ofertacursoid',
-                    'acpofertacurso.ocorrenciacursoid',
-                    'acpofertacurso.descricao as oferta_descricao',
-                    'acpofertacurso.situacao as oferta_situacao',
-                    'acpocorrenciacurso.cursoid',
-                    'acpocorrenciacurso.situacao as ocorrencia_situacao',
-                    'acpcurso.perfilcursoid',
-                    'acpcurso.numeroformalvagas',
-                    'acpcurso.datainicio as curso_datainicio',
-                    'acpcurso.datafim as curso_datafim',
-                    'acpperfilcurso.modelodeavaliacaogeral',
-                    'acpperfilcurso.modelodeavaliacaoseriado',
-                    'acpperfilcurso.modelodeavaliacaomodulo',
-                    'acpperfilcurso.permiteinscricaoporgrupo',
-                    'acpperfilcurso.descricao as perfil_descricao',
-                    'acpperfilcurso.ativo as perfil_ativo',
-                    'acpofertaturma.ofertaturmaid',
-                    'acpofertaturma.codigo as turma_codigo',
-                    'acpofertaturma.descricao as turma_descricao',
-                    'acpofertaturma.datainicialoferta',
-                    'acpofertaturma.datafinaloferta',
-                    'acpofertaturma.dataencerramento',
-                    'acpofertaturma.datainicialaulas',
-                    'acpofertaturma.datafinalaulas',
-                    'acpofertaturma.datainicialinscricao',
-                    'acpofertaturma.datafinalinscricao',
-                    'acpofertaturma.datainicialmatricula',
-                    'acpofertaturma.datafinalmatricula',
-                    'acpofertaturma.minimoalunos',
-                    'acpofertaturma.maximoalunos',
-                    'acpofertaturma.centerid',
-                    'acpofertaturma.unitid',
-                    'acpofertaturma.habilitada',
-                )
-                ->first();
+        if (!$cursoDados) throw new Exception("Dados do Curso insuficientes, verificar situação e período de oferta e turma");
 
-            if (!$cursoDados) throw new Exception("Dados do Curso insuficientes, verificar situação e período de oferta e turma");
+        // Matriz Curricular pode ter 1 ou mais resultados
+        $matrizCurricularDados = DB::table('public.acpmatrizcurricular')
+            ->where('public.acpmatrizcurricular.cursoid', $cursoDados->cursoid)
+            ->join(
+                'public.acpmatrizcurriculargrupo',
+                'public.acpmatrizcurricular.matrizcurricularid',
+                '=',
+                'public.acpmatrizcurriculargrupo.matrizcurricularid',
+            )
+            ->select(
+                'acpmatrizcurricular.matrizcurricularid',
+                'acpmatrizcurricular.componentecurricularid',
+                'acpmatrizcurricular.descricao as matrizcurricular_descricao',
+                'acpmatrizcurriculargrupo.matrizcurriculargrupoid',
+                'acpmatrizcurriculargrupo.descricao as matrizcurriculargrupo_descricao',
+            )
+            ->get();
 
-            // Matriz Curricular pode ter 1 ou mais resultados
-            $matrizCurricularDados = DB::table('public.acpmatrizcurricular')
-                ->where('public.acpmatrizcurricular.cursoid', $cursoDados->cursoid)
-                ->join(
-                    'public.acpmatrizcurriculargrupo',
-                    'public.acpmatrizcurricular.matrizcurricularid',
-                    '=',
-                    'public.acpmatrizcurriculargrupo.matrizcurricularid',
-                )
-                ->select(
-                    'acpmatrizcurricular.matrizcurricularid',
-                    'acpmatrizcurricular.componentecurricularid',
-                    'acpmatrizcurricular.descricao as matrizcurricular_descricao',
-                    'acpmatrizcurriculargrupo.matrizcurriculargrupoid',
-                    'acpmatrizcurriculargrupo.descricao as matrizcurriculargrupo_descricao',
-                )
-                ->get();
+        if (!$matrizCurricularDados) throw new Exception("Dados da Matriz Curricular insuficientes");
 
-            if (!$matrizCurricularDados) throw new Exception("Dados da Matriz Curricular insuficientes");
+        // Componente Curricular (ou Unidade) pode ter 1 ou mais resultados
+        $componenteCurricularDados = DB::table('public.acpofertacomponentecurricular')
+            ->where('public.acpofertacomponentecurricular.ofertaturmaid', $turmaId)
+            ->join(
+                'public.acpcomponentecurricularmatriz',
+                'public.acpofertacomponentecurricular.componentecurricularmatrizid',
+                '=',
+                'public.acpcomponentecurricularmatriz.componentecurricularmatrizid'
+            )
+            ->join(
+                'public.acpmatrizcurriculargrupo',
+                'public.acpcomponentecurricularmatriz.matrizcurriculargrupoid',
+                '=',
+                'public.acpmatrizcurriculargrupo.matrizcurriculargrupoid',
+            )
+            ->join(
+                'public.acpmatrizcurricular',
+                'public.acpmatrizcurriculargrupo.matrizcurricularid',
+                '=',
+                'public.acpmatrizcurricular.matrizcurricularid',
+            )
+            ->select(
+                'acpofertacomponentecurricular.ofertacomponentecurricularid',
+                'acpofertacomponentecurricular.componentecurricularmatrizid',
+                'acpcomponentecurricularmatriz.matrizcurriculargrupoid',
+                'acpmatrizcurriculargrupo.matrizcurricularid',
+                'acpmatrizcurricular.situacao',
+                'acpmatrizcurricular.descricao',
+            )
+            ->get();
 
-            // Componente Curricular (ou Unidade) pode ter 1 ou mais resultados
-            $componenteCurricularDados = DB::table('public.acpofertacomponentecurricular')
-                ->where('public.acpofertacomponentecurricular.ofertaturmaid', $turmaId)
-                ->join(
-                    'public.acpcomponentecurricularmatriz',
-                    'public.acpofertacomponentecurricular.componentecurricularmatrizid',
-                    '=',
-                    'public.acpcomponentecurricularmatriz.componentecurricularmatrizid'
-                )
-                ->join(
-                    'public.acpmatrizcurriculargrupo',
-                    'public.acpcomponentecurricularmatriz.matrizcurriculargrupoid',
-                    '=',
-                    'public.acpmatrizcurriculargrupo.matrizcurriculargrupoid',
-                )
-                ->join(
-                    'public.acpmatrizcurricular',
-                    'public.acpmatrizcurriculargrupo.matrizcurricularid',
-                    '=',
-                    'public.acpmatrizcurricular.matrizcurricularid',
-                )
-                ->select(
-                    'acpofertacomponentecurricular.ofertacomponentecurricularid',
-                    'acpofertacomponentecurricular.componentecurricularmatrizid',
-                    'acpcomponentecurricularmatriz.matrizcurriculargrupoid',
-                    'acpmatrizcurriculargrupo.matrizcurricularid',
-                    'acpmatrizcurricular.situacao',
-                    'acpmatrizcurricular.descricao',
-                )
-                ->get();
-
-            if (!$componenteCurricularDados) throw new Exception("Dados do Componente Curricular insuficientes");
-        } catch (\Throwable $e) {
-            throw new \Exception('Não foi possível encontrar todos os dados necessários: ' . $e->getMessage());
-        }
+        if (!$componenteCurricularDados) throw new Exception("Dados do Componente Curricular insuficientes");
 
         return [
             'cursoDados' => $cursoDados,
@@ -181,7 +171,6 @@ class CursoDAO
         $result = DB::table('public.acpinscricao')
             ->insertGetId(
                 [
-                    // 'username' => 'current_user()',
                     'datetime' => 'NOW()',
                     'ipaddress' => '127.0.0.1',
                     'personid' => $personid,
@@ -350,9 +339,9 @@ class CursoDAO
             }
 
             DB::commit();
-        } catch (\Throwable $e) {
+        } catch (Exception $e) {
             DB::rollback();
-            throw new \Exception('Não foi possível realizar a inscrição');
+            throw new Exception('Não foi possível realizar a inscrição. ' . $e->getMessage());
         }
 
         $result = [

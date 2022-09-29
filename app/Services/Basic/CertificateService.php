9ff94ee4959fbc89ee11439042e35066fb62795b
Jefferson Oliveira <jeff.doliveira1@outlook.com>
2022-09-29 14:03:05 -0300

    Template do certificado

--- app/Services/Basic/CertificateService.php
+++ app/Services/Basic/CertificateService.php
@@ -8,31 +8,83 @@ class CertificateService
 {
     public function generatePDF()
     {
-        $alunos = [
-            'aluno1',
-            'aluno2'
+        $course = [
+            "name" => "",
+            "period" => "",
+            "workload" => "",
+            "curriculum" => [
+                "name" => "",
+                [
+                    [
+                        "module" => "",
+                        "didactic_unit_code" => "",
+                        "didactic_unit" => ""
+                    ]
+                ]
+            ],
+            "students" => [
+                [
+                    "name" => ""
+                ]
+            ]
         ];
-
         $zip = new ZipArchive();
-        
-        $zip->open("/tmp/mpdf/certificates.zip", ZipArchive::CREATE);
-        
-        foreach ($alunos as $aluno) {
-            $mpdf = new \Mpdf\Mpdf(['tempDir' => '/tmp/mpdf']);
-            $html = "<h1>Hello {$aluno}!</h1>";
 
-            $mpdf->WriteHTML(utf8_encode($html));
+        $zip->open("/tmp/mpdf/{$course["name"]}.zip", ZipArchive::CREATE);
+
+        foreach ($course["students"] as $student) {
+            $defaultConfig = (new \Mpdf\Config\ConfigVariables())->getDefaults();
+            $fontDirs = $defaultConfig['fontDir'];
+            $defaultFontConfig = (new \Mpdf\Config\FontVariables())->getDefaults();
+            $fontData = $defaultFontConfig['fontdata'];
+
+            $mpdf = new \Mpdf\Mpdf([
+                'tempDir' => '/tmp/mpdf',
+                'mode' => 'utf-8',
+                'format' => [210, 297],
+                'orientation' => 'L',
+                'fontDir' => array_merge($fontDirs, [
+                    '/var/www/public/assets/fonts/archivo',
+                ]),
+                'fontdata' => $fontData + [
+                    'roboto' => [
+                        'R' => 'Archivo-Regular.ttf'
+                    ]
+                ]
+            ]);
+
+            $mpdf->SetImportUse();
+            $mpdf->SetDocTemplate('/var/www/public/assets/docs/certificate-template.pdf', true);
+            $mpdf->WriteHTML($this->PDFInfo());
 
             $content = $mpdf->Output('', 'S');
 
-            $zip->addFromString("{$aluno}.pdf", $content);
+            $zip->addFromString("{$student["name"]}.pdf", $content);
         }
 
         $zip->close();
 
+        header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
-        header("Content-Transfer-Encoding: utf-8");
-        header("Content-disposition: attachment; filename=\"" . basename('/tmp/mpdf/certificates.zip') . "\"");
+        header('Content-Disposition: attachment; filename="' . basename('/tmp/mpdf/certificates.zip') . '"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize('/tmp/mpdf/certificates.zip'));
         readfile('/tmp/mpdf/certificates.zip');
+        exit;
+    }
+
+    private function PDFInfo()
+    {
+        $info = [
+            'student_name' => '',
+            'course_name' => '',
+            'course_period' => '',
+            'course_workload' => '',
+            'today_date' => ''
+        ];
+
+        return view('certificate.info', $info);
     }
 }
